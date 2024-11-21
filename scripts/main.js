43a70cdf19c125c6d11d51f2d0c8d2ce617d37ca
diorm47 <dior47m@gmail.com>
2024-11-21 22:01:21 +0500

    mob menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,12 @@ function searchToggle() {
     .classList.toggle("active_search_wrapper");
 }
 
+function menuToggle() {
+  document
+    .querySelector(".mob_menu_wrapper")
+    .classList.toggle("visible_mob_menu_wrapper");
+}
+
 document.querySelectorAll(".faq_item").forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("faq_item_active");
